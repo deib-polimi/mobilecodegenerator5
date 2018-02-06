879e5bd8bbefd67cf0b60600c23cc1f5589a89dc
Massimo Beccari <massimo.beccari@gmail.com>
2018-02-06 14:44:11 +0100

    ANDROID: xml files and manifest completed

--- it.polimi.mobilecodegenerator/src/android_extensions/AndroidJavaUtil.java
+++ it.polimi.mobilecodegenerator/src/android_extensions/AndroidJavaUtil.java
@@ -1,5 +1,5 @@
 /************************************************************************************************************************
- * Copyright 2017 Gregorio Perego, Stefania Pezzetti, Aldo Pintus, Alessio Rossotti
+ * Copyright 2017 Gregorio Perego, Stefania Pezzetti, Aldo Pintus, Alessio Rossotti, Massimo Beccari
  * This file is part of it.polimi.mobilecodegenerator.
  * 
  * it.polimi.mobilecodegenerator is free software: 
